3193566044655297b32d5d5f431a28357030dee7
mgrinker@my.wctc.edu <>
2013-01-24 16:52:46 -0600

    removed comments and added print

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -1,7 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package firstgit;
 
 /**
@@ -14,6 +11,6 @@ public class FirstGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello World");
     }
 }
